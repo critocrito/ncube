18d71cff1d41f7b9372d992692a458503fa46787
Christo <christo@cryptodrunks.net>
2020-03-25 22:08:18 +0100

    feat: load a html page in the UI app from the HTTP background process

--- src/rs/ncube/src/main.rs
+++ src/rs/ncube/src/main.rs
@@ -1,3 +1,16 @@
+use web_view::*;
+
 fn main() {
-    println!("Hello, world!");
+    let port = 40666;
+
+    web_view::builder()
+        .title("Ncube")
+        .content(Content::Url(format!("http://127.0.0.1:{}", port)))
+        .size(1024, 800)
+        .resizable(true)
+        .debug(true)
+        .user_data(())
+        .invoke_handler(|_webview, _arg| Ok(()))
+        .run()
+        .unwrap();
 }
--- src/rs/ncubed/src/main.rs
+++ src/rs/ncubed/src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+use futures::join;
+use std::convert::Infallible;
+use tokio::sync::mpsc::{self, Receiver, Sender};
+use warp::Filter;
+
+fn with_tx(tx: Sender<()>) -> impl Filter<Extract = (Sender<()>,), Error = Infallible> + Clone {
+    warp::any().map(move || tx.clone())
+}
+
+async fn empty_reply(
+    mut tx: Sender<()>,
+    mut tx1: Sender<()>,
+) -> Result<impl warp::Reply, Infallible> {
+    tx.send(()).await.unwrap();
+    tx1.send(()).await.unwrap();
+    Ok(warp::reply())
+}
+
+async fn quit_signal(mut rx: Receiver<()>) {
+    rx.recv().await.unwrap();
+    println!("Quitting!");
+}
+
+#[tokio::main]
+async fn main() {
+    let (tx, rx) = mpsc::channel(1);
+    let (tx1, rx1) = mpsc::channel(1);
+
+    let routes = warp::get()
+        .and(warp::path::end())
+        .and(warp::fs::file("./public/index.html"));
+
+    let mgmt = warp::path!("quit")
+        .and(with_tx(tx))
+        .and(with_tx(tx1))
+        .and_then(empty_reply);
+
+    let (_addr, data_server) =
+        warp::serve(routes).bind_with_graceful_shutdown(([127, 0, 0, 1], 40666), quit_signal(rx));
+
+    let (_addr, mgmt_server) =
+        warp::serve(mgmt).bind_with_graceful_shutdown(([127, 0, 0, 1], 40667), quit_signal(rx1));
+
+    join!(data_server, mgmt_server);
 }
