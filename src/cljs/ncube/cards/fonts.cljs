df46de0d060c9c6cc25d9191f11b2d1bd11405f1
Christo <christo@cryptodrunks.net>
2020-05-01 11:19:53 +0200

    refactor(ui): add correct text scales

--- src/cljs/ncube/cards/fonts.cljs
+++ src/cljs/ncube/cards/fonts.cljs
@@ -8,16 +8,44 @@
 
 (defcard header-fonts
   (r/as-element
-   [:div {:class ["flex flex-column"]}
-    [:h1 {:class ["fh1"]} "Header One"]
-    [:h2 {:class ["fh2"]} "Header Two"]
-    [:h3 {:class ["fh3"]} "Header Three"]
-    [:h4 {:class ["fh4"]} "Header Four"]
-    [:h5 {:class ["fh5"]} "Header Five"]]))
+   [:div {:class ["pa4 bg-buzzy-boop flex flex-column"]}
+    [:h1 {:class ["back-to-reality header1"]} "Header One"]
+    [:h2 {:class ["back-to-reality header2"]} "Header Two"]
+    [:h3 {:class ["back-to-reality header3"]} "Header Three"]
+    [:h4 {:class ["back-to-reality header4"]} "Header Four"]
+    [:h5 {:class ["back-to-reality header5"]} "Header Five"]]))
 
-(defcard body-fonts
+(defcard text
   (r/as-element
-   [:div {:class ["flex flex-column"]}
-    [:p {:class ["fb1"]} "A modular scale, like a musical scale, is a prearranged set of harmonious proportions."]
-    [:p {:class ["fb2"]} "A modular scale, like a musical scale, is a prearranged set of harmonious proportions."]
-    [:p {:class ["fb3"]} "A modular scale, like a musical scale, is a prearranged set of harmonious proportions."]]))
+   [:div {:class ["bg-buzzy-boop pa4"]}
+    [:p "Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et."]]))
+
+(defcard text-bold
+  (r/as-element
+   [:div {:class ["bg-buzzy-boop pa4"]}
+    [:p {:class ["b"]} "Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et."]]))
+
+(defcard text-uppercase-bold
+  (r/as-element
+   [:div {:class ["bg-buzzy-boop pa4"]}
+    [:p {:class ["b ttu"]} "Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et."]]))
+
+(defcard text-medium
+  (r/as-element
+   [:div {:class ["bg-buzzy-boop pa4"]}
+    [:p {:class ["text-medium"]} "Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et."]]))
+
+(defcard text-medium-bold
+  (r/as-element
+   [:div {:class ["bg-buzzy-boop pa4"]}
+    [:p {:class ["text-medium b"]} "Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et."]]))
+
+(defcard text-small
+  (r/as-element
+   [:div {:class ["bg-buzzy-boop pa4"]}
+    [:p {:class ["text-small"]} "Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et."]]))
+
+(defcard text-tiny
+  (r/as-element
+   [:div {:class ["bg-buzzy-boop pa4"]}
+    [:p {:class ["text-tiny"]} "Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et. Cumque vel corporis cum animi rerum. Adipisci fuga voluptas temporibus maxime. Nihil praesentium accusantium et."]]))
