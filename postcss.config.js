9762c0d8eb3d23ec3a9c21bf1e51b6d8f0ec3f6f
Christo <christo@cryptodrunks.net>
2020-05-04 02:34:22 +0200

    chore: move postcss config into package.json

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: {
-    "postcss-import": {},
-    "postcss-insert": {},
-    "postcss-custom-properties": { preserve: false },
-    "postcss-preset-env": { browsers: "> 0.2%" },
-    autoprefixer: {}
-  }
-};
