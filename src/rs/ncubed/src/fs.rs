b654d5aeaf33f812125d060c6e699248f63d4c08
Christo <christo@cryptodrunks.net>
2020-05-05 15:38:27 +0200

    feat(ncubed): build a workspace archive file including NodeJS
    fixes #22

--- src/rs/ncubed/src/fs.rs
+++ src/rs/ncubed/src/fs.rs
@@ -1,10 +1,11 @@
 use dirs;
+use flate2::read::GzDecoder;
 use std::convert::AsRef;
 use std::fmt::Debug;
 use std::fs;
 use std::path::{Path, PathBuf};
+use tar::Archive;
 use tracing::{debug, instrument};
-use zip::ZipArchive;
 
 use crate::errors::HostError;
 
@@ -51,48 +52,17 @@ pub(crate) fn mkdirp<P: AsRef<Path> + Debug>(target: P) -> Result<(), HostError>
 pub(crate) fn unzip_workspace<P: AsRef<Path> + Debug>(target: P) -> Result<(), HostError> {
     let expanded_target =
         expand_tilde(target).ok_or(HostError::General("Failed to expand path".into()))?;
+
+    debug!("Extract workspace archive to {:?}", expanded_target);
+
     let mut v = Vec::new();
-    v.extend_from_slice(include_bytes!("../../../../target/workspace.zip"));
-    let reader = std::io::Cursor::new(v);
-    let mut archive = ZipArchive::new(reader).map_err(|err| HostError::General(err.to_string()))?;
-
-    for i in 0..archive.len() {
-        let mut file = archive.by_index(i).unwrap();
-        let outpath = expanded_target.join(file.sanitized_name());
-
-        if (&*file.name()).ends_with('/') {
-            debug!(
-                "File {} extracted to \"{}\"",
-                i,
-                outpath.as_path().display()
-            );
-            fs::create_dir_all(&outpath).unwrap();
-        } else {
-            debug!(
-                "File {} extracted to \"{}\" ({} bytes)",
-                i,
-                outpath.as_path().display(),
-                file.size()
-            );
-            if let Some(p) = outpath.parent() {
-                if !p.exists() {
-                    fs::create_dir_all(&p).unwrap();
-                }
-            }
-            let mut outfile = fs::File::create(&outpath).unwrap();
-            std::io::copy(&mut file, &mut outfile).unwrap();
-        }
-
-        // Get and Set permissions
-        #[cfg(unix)]
-        {
-            use std::os::unix::fs::PermissionsExt;
-
-            if let Some(mode) = file.unix_mode() {
-                fs::set_permissions(&outpath, fs::Permissions::from_mode(mode)).unwrap();
-            }
-        }
-    }
+    v.extend_from_slice(include_bytes!(
+        "../../../../target/workspace/workspace.tar.gz"
+    ));
+    let tar_gz = std::io::Cursor::new(v);
+    let tar = GzDecoder::new(tar_gz);
+    let mut archive = Archive::new(tar);
+    archive.unpack(expanded_target)?;
 
     Ok(())
 }
