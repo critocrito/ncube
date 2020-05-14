cab9e4e002424911785125ab52009c57623850a3
Christo <christo@cryptodrunks.net>
2020-05-14 17:26:28 +0200

    feat(ncubed): register workspace databases on the host

---
+++ src/rs/ncubed/src/sql/workspace/create_database.sql
@@ -0,0 +1,9 @@
+INSERT INTO workspace_database (
+  workspace,
+  kind,
+  path
+) VALUES (
+  ?1,
+  ?2,
+  ?3
+);
--- src/rs/ncubed/src/sql/workspace/show_by_slug.sql
+++ src/rs/ncubed/src/sql/workspace/show_by_slug.sql
@@ -1,11 +1,15 @@
 SELECT
-  id,
-  name,
-  slug,
-  description,
-  kind,
-  location,
-  created_at,
-  updated_at
-FROM workspace
+  w.id AS id,
+  w.name AS name,
+  w.slug AS slug,
+  w.description AS description,
+  w.kind AS kind,
+  w.location AS location,
+  w.created_at AS created_at,
+  w.updated_at AS updated_at,
+  wd.kind AS database,
+  wd.path AS database_path
+FROM workspace AS w
+LEFT JOIN workspace_database AS wd
+     ON w.id = wd.workspace
 WHERE slug = ?1;
