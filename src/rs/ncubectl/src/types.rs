8f0b22449ec02a5bea6e5015e0abbdc8b3cdf06b
Christo <christo@cryptodrunks.net>
2020-06-09 15:27:52 +0200

    fix(ncubed): include remote workspace endpoint in the connection object

--- src/rs/ncubectl/src/types.rs
+++ src/rs/ncubectl/src/types.rs
@@ -4,6 +4,7 @@ use serde::Serialize;
 #[derive(Debug, Serialize)]
 pub(crate) struct ConnectionOut {
     pub(crate) workspace: String,
+    pub(crate) endpoint: String,
     pub(crate) description: Option<String>,
     pub(crate) email: String,
     pub(crate) otp: Option<String>,
