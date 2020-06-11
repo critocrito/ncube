791bd0422a6ad7d7bbcf76e53604d4f4ec9499f3
Christo <christo@cryptodrunks.net>
2020-06-11 22:34:30 +0200

    refactor(ncubed): move tasks into it's own file

--- src/rs/ncubed/src/actors/task.rs
+++ src/rs/ncubed/src/actors/task.rs
@@ -1,9 +1,11 @@
 use async_trait::async_trait;
+use std::fmt::Debug;
 use tokio::sync::mpsc::{self, Sender};
 use xactor::{message, Actor, Context, Handler};
 
 use crate::actors::Registry;
 use crate::errors::{ActorError, HostError};
+use crate::tasks::create_workspace;
 
 #[derive(Debug)]
 enum TaskMessage {
@@ -12,65 +14,6 @@ enum TaskMessage {
 
 pub(crate) struct TaskActor {
     tx: Sender<TaskMessage>,
-}
-
-mod tasks {
-    use std::fmt::Debug;
-    use std::path::Path;
-    use tokio::process::Command;
-    use tracing::{debug, info, instrument};
-
-    use crate::errors::HostError;
-    use crate::fs::{expand_tilde, mkdirp, unzip_workspace};
-
-    #[instrument]
-    pub(crate) async fn create_workspace<P: AsRef<Path> + Debug>(
-        location: P,
-    ) -> Result<(), HostError> {
-        let expanded_path = expand_tilde(location)
-            .ok_or_else(|| HostError::General("Failed to expand path".into()))?;
-
-        mkdirp(&expanded_path)?;
-        unzip_workspace(&expanded_path)?;
-
-        let env_path = vec![
-            "dist/nodejs/bin",
-            "dist/ffmpeg",
-            "dist/youtube-dl",
-            "node_modules/.bin:/usr/local/bin:/usr/bin:/bin",
-        ]
-        .iter()
-        .map(|s| format!("{}/{}", expanded_path.as_path().to_string_lossy(), s))
-        .collect::<Vec<String>>()
-        .join(":");
-
-        debug!("PATH={}", env_path);
-
-        Command::new("npm")
-            .current_dir(expanded_path.clone())
-            .env("PATH", &env_path)
-            .arg("i")
-            .spawn()
-            .expect("npm failed to start")
-            .await
-            .expect("npm failed to run");
-
-        info!("Installed Sugarcube dependencies.",);
-
-        Command::new("sugarcube")
-            .current_dir(expanded_path.clone())
-            .env("PATH", &env_path)
-            .arg("-p")
-            .arg("sql_schema_migrate")
-            .spawn()
-            .expect("npm failed to start")
-            .await
-            .expect("npm failed to run");
-
-        info!("Migrated the Sqlite database.",);
-
-        Ok(())
-    }
 }
 
 impl Actor for TaskActor {}
@@ -84,11 +27,9 @@ impl TaskActor {
         tokio::spawn(async move {
             while let Some(res) = rx.recv().await {
                 match res {
-                    TaskMessage::SetupWorkspace(location) => {
-                        tasks::create_workspace(location)
-                            .await
-                            .expect("Failed to create workspace");
-                    }
+                    TaskMessage::SetupWorkspace(location) => create_workspace(location)
+                        .await
+                        .expect("Failed to create workspace"),
                 }
             }
         });
