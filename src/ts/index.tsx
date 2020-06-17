923b85f28a5d4f32458cf227d8c86eeb1c2b1155
Christo <christo@cryptodrunks.net>
2020-06-17 20:42:42 +0200

    refactor(ui): add scaffold for client routing

--- src/ts/index.tsx
+++ src/ts/index.tsx
@@ -3,17 +3,8 @@ import "../css/styles.css";
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Counter from "./counter";
-
-const App = () => {
-  return (
-    <div>
-      <p>Hello World</p>
-      <Counter cnt={2} />
-    </div>
-  );
-};
+import Routes from "./routes";
 
 const domContainer = document.querySelector("#app");
 
-ReactDOM.render(<App />, domContainer);
+ReactDOM.render(<Routes />, domContainer);
