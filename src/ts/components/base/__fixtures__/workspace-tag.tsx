deb76b296267c68291ae65aac001db428670d551
Christo <christo@cryptodrunks.net>
2020-06-17 16:55:07 +0200

    refactor: move base fixtures to different location

--- src/ts/components/base/__fixtures__/workspace-tag.tsx
+++ src/ts/components/base/__fixtures__/workspace-tag.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import WorkspaceTag from "../base/workspace-tag";
+import WorkspaceTag from "../workspace-tag";
 
 export default (
   <div className="noto lh-copy pa2 flex flex-column">
