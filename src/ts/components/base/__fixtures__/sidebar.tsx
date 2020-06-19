a770fbb2ac642e3728ec37639ac96ce92eebc069
Christo <christo@cryptodrunks.net>
2020-06-19 16:44:19 +0200

    refactor(ui): add a sidebar in typescript

---
+++ src/ts/components/base/__fixtures__/sidebar.tsx
@@ -0,0 +1,50 @@
+import {parseISO} from "date-fns";
+import React from "react";
+
+import {Workspace} from "../../../types";
+import Sidebar from "../sidebar";
+
+const workspaces: Workspace[] = [
+  {
+    id: 1,
+    name: "Syrian Archive",
+    description: "",
+    slug: "syrian-archive",
+    kind: "local",
+    location: "~/Ncube/syrian-archive",
+    database: "sqlite",
+    database_path: "~/Ncube/syrian-archive/sugarcube.db",
+    created_at: parseISO("2020-06-19T11:00:09.856432Z"),
+    updated_at: parseISO("2020-06-19T11:00:09.856432Z"),
+  },
+  {
+    id: 2,
+    name: "Yemen Archive",
+    description: "",
+    slug: "yemen-archive",
+    kind: "local",
+    location: "~/Ncube/yemen-archive",
+    database: "sqlite",
+    database_path: "~/Ncube/yemen-archive/sugarcube.db",
+    created_at: parseISO("2020-06-19T11:00:09.856432Z"),
+    updated_at: parseISO("2020-06-19T11:00:09.856432Z"),
+  },
+  {
+    id: 2,
+    name: "A workspace with a really long name",
+    description: "",
+    slug: "long-name-archive",
+    kind: "local",
+    location: "~/Ncube/yemen-archive",
+    database: "sqlite",
+    database_path: "~/Ncube/yemen-archive/sugarcube.db",
+    created_at: parseISO("2020-06-19T11:00:09.856432Z"),
+    updated_at: parseISO("2020-06-19T11:00:09.856432Z"),
+  },
+];
+
+export default (
+  <div className="noto lh-copy flex flex-column">
+    <Sidebar workspaces={workspaces} />
+  </div>
+);
