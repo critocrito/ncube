a41b2f737b0e76c034fddadacb1864c65cef4a04
Christo <christo@cryptodrunks.net>
2020-07-28 22:34:37 +0200

    refactor(ui): enable form focus rings only when tabbing

--- src/ts/client.tsx
+++ src/ts/client.tsx
@@ -5,6 +5,18 @@ import ReactDOM from "react-dom";
 
 import App from "./app";
 
+// enable form focus rings when tabbing
+// see: https://medium.com/hackernoon/removing-that-ugly-focus-ring-and-keeping-it-too-6c8727fefcd2
+const handleFirstTab = (ev: KeyboardEvent) => {
+  // the "I am a keyboard user" key
+  if (ev.keyCode === 9) {
+    document.body.classList.add("user-is-tabbing");
+    window.removeEventListener("keydown", handleFirstTab);
+  }
+};
+
+window.addEventListener("keydown", handleFirstTab);
+
 const domContainer = document.querySelector("#app");
 
 ReactDOM.render(<App />, domContainer);
