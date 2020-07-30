500341707c8615931db424a28b51601da5b8c692
Christo <christo@cryptodrunks.net>
2020-07-30 23:14:32 +0200

    refactor(web-ext): enable form focus rings only when tabbing

--- src/ts/discovery.tsx
+++ src/ts/discovery.tsx
@@ -8,6 +8,18 @@ import Layout from "./popup/layout";
 import Popup from "./popup/popup";
 import {currentUrl} from "./popup/utils";
 
+// enable form focus rings when tabbing
+// see: https://medium.com/hackernoon/removing-that-ugly-focus-ring-and-keeping-it-too-6c8727fefcd2
+const handleFirstTab = (ev: KeyboardEvent) => {
+  // the "I am a keyboard user" key
+  if (ev.keyCode === 9) {
+    document.body.classList.add("user-is-tabbing");
+    window.removeEventListener("keydown", handleFirstTab);
+  }
+};
+
+window.addEventListener("keydown", handleFirstTab);
+
 (async () => {
   const domContainer = document.querySelector("#app");
   const url = await currentUrl();
