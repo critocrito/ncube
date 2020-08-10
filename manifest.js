3cc89d21b707bd7c9a4c1597338d64b01f477c17
Christo <christo@cryptodrunks.net>
2020-08-10 20:10:20 +0200

    refactor(discovery): replace logos

--- manifest.js
+++ manifest.js
@@ -1,25 +1,20 @@
 module.exports = {
   name: "ncube-discovery",
 
-  version: "0.0.1",
+  version: "0.1.0",
 
   manifest_version: 2,
   icons: {
-    "48": "icons/sd-48.png",
+    "48": "icons/logo.png",
+    "96": "icons/logo@2x.png",
   },
 
   permissions: ["activeTab"],
 
   browser_action: {
     browser_style: true,
-    default_icon: "icons/sd-48.png",
+    default_icon: "icons/logo.png",
     default_title: "Ncube Discovery",
     default_popup: "popup.html",
-  },
-
-  applications: {
-    gecko: {
-      id: "christo@cryptodrunks.net",
-    },
   },
 };
