9cb39834fd8706bb0baff9d67fef051ce3aebc86
Christo <christo@cryptodrunks.net>
2020-08-10 19:40:38 +0200

    feat(discovery): cross browser compatibility for Google Chrome

--- src/ts/popup/utils.ts
+++ src/ts/popup/utils.ts
@@ -1,3 +1,5 @@
+import browser from "webextension-polyfill";
+
 export const currentUrl = async (): Promise<string> => {
   const tabs = await browser.tabs.query({currentWindow: true, active: true});
   if (tabs.length === 0) {
