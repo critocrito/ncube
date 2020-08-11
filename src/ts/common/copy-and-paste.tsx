6d554b8f50e0b088df0b5c40c8b329728b05f3ce
Christo <christo@cryptodrunks.net>
2020-08-11 10:00:19 +0200

    feat(ui): add visual queue when copying to clipboard

--- src/ts/common/copy-and-paste.tsx
+++ src/ts/common/copy-and-paste.tsx
@@ -1,5 +1,6 @@
+import c from "classnames";
 import copy from "copy-to-clipboard";
-import React from "react";
+import React, {useState} from "react";
 
 import iconClipboard from "../../../resources/public/images/icon_clipboard.svg";
 
@@ -8,15 +9,26 @@ interface CopyAndPasteProps {
 }
 
 const CopyAndPaste = ({value}: CopyAndPasteProps) => {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const copyHandler = () => {
+    copy(value);
+    setIsCopied(true);
+    setTimeout(() => setIsCopied(false), 3000);
+  };
+
+  const copyLabelStyle = c("ttu f7", isCopied ? "visible" : "fade-out");
+
   return (
-    <button
-      className="b--none bg-transparent"
-      onClick={() => {
-        copy(value);
-      }}
-    >
-      <img src={iconClipboard} alt="copy to clipboard." className="h1 w1" />
-    </button>
+    <div className="flex items-center">
+      <span className={copyLabelStyle}>Copied!</span>
+      <button
+        onClick={copyHandler}
+        className="bg-transparent ba br2 bw0 ml2 dim"
+      >
+        <img src={iconClipboard} alt="copy to clipboard." className="h1 w1" />
+      </button>
+    </div>
   );
 };
 
