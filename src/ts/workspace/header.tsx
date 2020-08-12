4018274d23a5e1afbd80e21feb589708e0d17ab1
Christo <christo@cryptodrunks.net>
2020-08-13 00:48:31 +0200

    feat(ui): go to workspace overview when clicking on breadcrumb

--- src/ts/workspace/header.tsx
+++ src/ts/workspace/header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import WorkspaceTag from "../common/workspace-tag";
+import {useWorkspaceCtx} from "../context";
 import {Workspace} from "../types";
 
 interface HeaderProps {
@@ -8,9 +9,18 @@ interface HeaderProps {
 }
 
 const Header = ({workspace}: HeaderProps) => {
+  const [, workspaceSend] = useWorkspaceCtx();
+
   return (
     <div className="bb b--sapphire w-100 flex justify-between items-center pb2">
-      <div className="b text-medium sapphire ttu w-30">{`Workspace: ${workspace.name}`}</div>
+      <div className="b text-medium sapphire ttu w-30">
+        <button
+          className="b--none bg-transparent pointer b sapphire ttu ma0 pa0"
+          onClick={() => workspaceSend("OVERVIEW")}
+        >
+          {`Workspace: ${workspace.name}`}
+        </button>
+      </div>
       <WorkspaceTag className="mb1" kind={workspace.kind} />
     </div>
   );
