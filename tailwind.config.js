6729d9611dc5a490a25abad7d6aa788c8adc98af
Christo <christo@cryptodrunks.net>
2021-04-14 13:31:52 +0200

    chore: integrate TailwindCSS stack
    I'm trying to run Tachyons and TailwindCSS in parallel. This is really
    not perfect but workable for now. Some styles and layouts are obviously
    messed up.
    
    refs #67

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./src/ts/**/*.{js,ts,jsx,tsx}", "./resources/public/**/*.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
