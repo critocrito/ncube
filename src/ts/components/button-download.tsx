99f31f251c3f8cdfcc9eda52572f6e3a540f64c4
Christo <christo@cryptodrunks.net>
2021-05-30 16:14:11 +0000

    feat(ui): download media files in data details (#73)
    * feat(ui): download media files in data details
    
    This commit adds the ability to the media viewer to download individual
    media files while inspecting data details.
    
    * fix: pass linting and handle possible undefined

--- src/ts/components/button-download.tsx
+++ src/ts/components/button-download.tsx
@@ -5,18 +5,28 @@ import iconDownload from "../../../resources/public/images/icon_download.svg";
 
 interface ButtonDownloadProps {
   onClick: () => Promise<void>;
-  disabled: boolean;
+  disabled?: boolean;
   label?: string;
+  className?: string;
 }
 
-const ButtonDownload = ({onClick, disabled, label}: ButtonDownloadProps) => {
-  const className = c("border-none bg-transparent", {
-    "pointer-events-none opacity-50": disabled,
-  });
+const ButtonDownload = ({
+  onClick,
+  disabled = false,
+  label,
+  className,
+}: ButtonDownloadProps) => {
+  const classes = c(
+    "flex items-center border-none bg-transparent",
+    {
+      "pointer-events-none opacity-50": disabled,
+    },
+    className,
+  );
 
   return (
-    <button className={className} onClick={onClick}>
-      {label && <span>label</span>}
+    <button className={classes} onClick={onClick}>
+      {label && <span>{label}</span>}
       <img src={iconDownload} className="h-5 w-5" alt="download file" />
     </button>
   );
--- src/ts/components/media-viewer.tsx
+++ src/ts/components/media-viewer.tsx
@@ -3,9 +3,11 @@ import {Swiper as SwiperType} from "swiper";
 import {Swiper, SwiperSlide} from "swiper/react";
 
 import {useWorkspaceCtx} from "../lib/context";
+import {downloadAsFile} from "../lib/utils";
 import chevronLeft from "../svg/chevron_left.svg";
 import chevronRight from "../svg/chevron_right.svg";
 import {Download} from "../types";
+import ButtonDownload from "./button-download";
 import VideoPlayer from "./video-player";
 
 interface MediaViewerProps {
@@ -16,6 +18,25 @@ interface SlideProps {
   url: string;
   type: string;
 }
+
+const posterForVideo = (url: string) =>
+  `${url.slice(0, Math.max(0, url.lastIndexOf(".")))}.jpg`;
+
+const downloadMedia = async (url: string): Promise<void> => {
+  // By default fetch uses CORS mode. But when server response doesn't contain
+  // 'Access-Control-Allow-Origin' header. It skips response body. Ironically,
+  // you have to set mode as 'no-cors' to request opaque resources. Opaque
+  // responses can't be accessed with JavaScript but the response can still be
+  // served or cached by a service worker.
+  const resp = await fetch(url, {mode: "no-cors"});
+  const blob = await resp.blob();
+
+  const filename = url.slice(Math.max(0, url.lastIndexOf("/") + 1));
+  const mimeType =
+    resp.headers.get("Content-Type") || "application/octet-stream";
+
+  downloadAsFile(mimeType, filename, blob);
+};
 
 const MediaSlide = ({url, type}: SlideProps) => {
   switch (type) {
@@ -39,7 +60,7 @@ const ThumbSlide = ({url, type}: SlideProps) => {
         />
       );
     case "video": {
-      const poster = `${url.slice(0, Math.max(0, url.lastIndexOf(".")))}.jpg`;
+      const poster = posterForVideo(url);
       return (
         <img className="cursor-pointer z-10" src={poster} alt="Video poster." />
       );
@@ -71,8 +92,8 @@ const MediaViewer = ({downloads}: MediaViewerProps) => {
   if (downloads.length === 0) return <div />;
 
   return (
-    <div className="relative w-full flex items-center">
-      <div className="w-full">
+    <div className="relative w-full flex items-center py-5">
+      <div className="w-full space-y-2">
         <Swiper
           slidesPerView={1}
           onSwiper={setSwiper}
@@ -81,9 +102,15 @@ const MediaViewer = ({downloads}: MediaViewerProps) => {
         >
           {downloads.map(({id_hash: idHash, location, type}) => {
             const url = `http://127.0.0.1:40666/api/workspaces/${slug}/${location}`;
+            const handleClick = async () => downloadMedia(url);
 
             return (
               <SwiperSlide key={idHash}>
+                <ButtonDownload
+                  className="ml-auto"
+                  onClick={handleClick}
+                  label="Download Media"
+                />
                 <MediaSlide key={idHash} url={url} type={type} />
               </SwiperSlide>
             );
